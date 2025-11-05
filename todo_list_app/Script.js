eee9c50e8142b5fa3f2589de1b9460b342eee56c
Dinesh Rout <rddinesh779@gmail.com>
2025-11-06 01:16:37 +0530

    fix: UI fixes

--- todo_list_app/Script.js
+++ todo_list_app/Script.js
@@ -1,45 +1,44 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
 function addTask() {
-  if (inputBox.value.trim() === "") {
-    alert("You must give some task.");
+  if (inputBox.value.trim() === '') {
+    alert('You must give some task.');
   } else {
-    let it = document.createElement("li"); // create list
-    it.innerHTML = inputBox.value; // put value in list
-    listContainer.appendChild(it); //add li in ul list-container
+    let listItem = document.createElement('li'); // create list
+    listItem.innerHTML = inputBox.value; // put value in list
+    listContainer.appendChild(listItem); //add li in ul list-container
+    console.log('todo value added');
 
     //add edit button
-    let editspn = document.createElement("span");
-    console.log("edit");
-    editspn.innerText = "E";
-    editspn.classList.add("edit");
-    it.appendChild(editspn);
-    console.log("edit added");
+    let editspn = document.createElement('span');
+    editspn.innerText = '✏️';
+    editspn.classList.add('edit');
+    listItem.appendChild(editspn);
+    console.log('edit button added');
 
     //add delete button
-    let span = document.createElement("span");
-    span.innerHTML = "\u00d7";
-    it.appendChild(span);
-
-    // listContainer.appendChild(it);
+    let deletespan = document.createElement('span');
+    deletespan.innerHTML = '❌';
+    listItem.appendChild(deletespan);
+    console.log('delete button added');
   }
-  inputBox.value = "";
+  inputBox.value = '';
   saveData();
 }
 
 listContainer.addEventListener(
-  "click",
+  'click',
   function (e) {
-    if (e.target.tagName === "LI") {
-      e.target.classList.toggle("checked");
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked');
       saveData();
     } else if (
-      e.target.tagName === "SPAN" &&
-      !e.target.classList.contains("edit")
+      e.target.tagName === 'SPAN' &&
+      !e.target.classList.contains('edit')
     ) {
       e.target.parentElement.remove();
       saveData();
-    } else if (e.target.classList.contains("edit")) {
+    } else if (e.target.classList.contains('edit')) {
       let listItem = e.target.parentElement;
       handleEdit(listItem);
     }
@@ -51,17 +50,19 @@ function handleEdit(listItem) {
   //get the current text
   let newTesKNode = listItem.firstChild;
   //update the text and save
-  let newText = prompt("Edit your task:", newTesKNode.textContent);
-  if (newText !== null && newText.trim() !== "") {
+  let newText = prompt('Edit your task:', newTesKNode.textContent);
+  if (newText !== null && newText.trim() !== '') {
     newTesKNode.textContent = newText.trim();
     saveData(); //assume this saves the updated list to localstorage.
   }
 }
 
 function saveData() {
-  localStorage.setItem("data", listContainer.innerHTML);
+  localStorage.setItem('data', listContainer.innerHTML);
 }
+
 function showtask() {
-  listContainer.innerHTML = localStorage.getItem("data");
+  listContainer.innerHTML = localStorage.getItem('data');
 }
+
 showtask();
